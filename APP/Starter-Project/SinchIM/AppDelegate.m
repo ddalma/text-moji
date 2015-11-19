9cb955e46ef9b8e560c69bc4b3b4586e38574bc3
Andrew Caputi <acaputi@optonline.net>
2015-11-18 22:27:55 -0500

    initial commit for app project. TODO: need to iron out a proper gitignore based on what file changes don't affect the build (probably anyhing in xcuserdata), and what is recommended online.

---
+++ APP/Starter-Project/SinchIM/AppDelegate.m
@@ -0,0 +1,72 @@
+#import "AppDelegate.h"
+
+
+
+@interface AppDelegate () <SINServiceDelegate>
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    id config = [SinchService configWithApplicationKey: @"<YOUR__APP_KEY">
+                                     applicationSecret: @"<YOUR_APP_SECRET">
+                                       environmentHost: @"sandbox.sinch.com"
+                      pushNotificationsWithEnvironment: SINAPSEnvironmentAutomatic];
+    id<SINService> sinch = [SinchService serviceWithConfig:config];
+    sinch.delegate = self;
+    
+    void(^onUserDidLogin)(NSString * ) = ^(NSString *userId){
+        [sinch logInUserWithId: userId];
+        [sinch.push setDesiredPushTypeAutomatically];
+        [sinch.push registerUserNotificationSettings];
+    };
+    self.sinch = sinch;
+    
+    [[NSNotificationCenter defaultCenter]
+        addObserverForName:@"UserDidLoginNotification"
+        object:nil
+        queue:nil
+        usingBlock:^(NSNotification * note) {
+            onUserDidLogin(note.userInfo[@"userId"]);}];
+        }
+     
+     
+     ]
+    
+  return YES;
+}
+
+
+#pragma mark - SINServiceDelegate
+
+-(void) service:(id<SINService>)service didFailWithError:(NSError *)error{
+    NSLog(@"%@",[error localizedDescription]);
+}
+
+-(void)service:(id<SINService>)service
+    logMessage:(NSString *)message
+          area:(NSString *)area
+      severity:(SINLogSeverity)severity
+      timestap:(NSDate *)timestamp{
+    if(severity == SINLogSeverityCritical){
+        NSLog(@"%@", message);
+    }
+}
+
+
+
+#pragma mark - push
+
+-(void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    [self.sinch.push application:application didRegisterForRemoteNotificationsWithDeviceToken: deviceToken];
+}
+
+-(void)application:(UIApplication *)application  didReceiveRemoteNotification:(NSDictionary *)userInfo{
+    [self.sinch.push application: application didRecieveRemoteNotification: userInfo];
+}
+
+-(void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error{
+    NSLog(@"%@:%@", NSStringFromSelector(_cmd), error);
+}
+
+@end
